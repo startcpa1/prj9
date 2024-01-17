d2c8a21fc4ee7e821c792a7a3c507f93e4e4da5d
Aleksandr Podcherednik <startcpa1@yandex.ru>
2024-01-17 22:03:02 +0300

    объединил прошлый и текущий проекты в один

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+
+
+def index(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print(f'You have new message from {name}({email}): {message}')
+    return render(request, 'catalog/index.html')
+
+
+def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print(f'You have new message from {name}({email}): {message}')
+    return render(request, 'catalog/contact.html')
--- config/urls.py
+++ config/urls.py
@@ -14,9 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+from catalog.views import contacts, index
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', index, name='index'),
+    path('contacts/', contacts, name='contacts')
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
